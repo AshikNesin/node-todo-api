0610368437f6d40733e0f8103ba41b021848b7c3
Ashik Nesin <mail@ashiknesin.com>
2017-02-18 17:29:34 +0530

    Add express and todo endpoint

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const {mongoose} = require('./db/mongoose')
+const {Todo} = require('./models/todo')
+const {User} = require('./models/user')
+
+app.use(bodyParser.json())
+
+app.post('/todos',(req,res)=>{
+	const todo = new Todo({
+		text:req.body.text
+	})
+	todo.save().then((doc)=>{
+		res.send(doc)
+	},(err)=>{
+		res.status(400).send(err)
+	})
+})
+app.listen(3000,()=>{
+	console.log('Todo API running on PORT 3000');
+})
