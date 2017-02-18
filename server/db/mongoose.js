cf30978db2d53ed8947c24540b148eef6552d24b
Ashik Nesin <mail@ashiknesin.com>
2017-02-18 19:44:21 +0530

    Add MONGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/node-todo')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/node-todo')
 module.exports = {mongoose}
