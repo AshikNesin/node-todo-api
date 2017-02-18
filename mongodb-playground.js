5fd5e62f5d2f34637c8ce1e358284d499a3ad9f4
Ashik Nesin <mail@ashiknesin.com>
2017-02-18 12:55:55 +0530

    Init Commit

---
+++ mongodb-playground.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost:27017/testdb',(err,db)=>{
+	if(err){
+		return console.log('Unable to connect to DB');
+	}
+	console.log('Mongodb Connected');
+	// db.collection('Todo').insertOne({
+	// 	text:'Its a completed task',
+	// 	completed:true
+	// },(err,result)=>{
+	// 	if(err){
+	// 		return console.log('Not able to write');
+	// 	}
+	// 	console.log(JSON.stringify(result.ops));
+	// })
+
+
+	// db.collection('Todo').find({completed:false}).toArray().then((docs)=>{
+	// 	console.log(docs);
+	// },(err)=>{
+	// 	console.log(err);
+	// })
+
+	// db.collection('Todo').deleteMany({completed:false}).then((res)=>{
+	// 	console.log(res);
+	// })
+
+	db.collection('Todo').findOneAndUpdate({
+		completed:true
+	},{
+		$set:{
+			completed:false
+		}
+	})
+
+	db.close()
+})
