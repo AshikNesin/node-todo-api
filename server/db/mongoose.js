54be014cfa750519c8e8291757cd33eb38a6dc64
Ashik Nesin <mail@ashiknesin.com>
2017-02-18 21:46:45 +0530

    Add test db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/node-todo')
+const MONGODB_URI = {
+	dev:'mongodb://localhost:27017/node-todo',
+	test:'mongodb://localhost:27017/node-todo-test',
+	production:process.env.MONGODB_URI
+}
+mongoose.connect(MONGODB_URI[process.env.NODE_ENV])
+console.log(MONGODB_URI[process.env.NODE_ENV]);
 module.exports = {mongoose}
